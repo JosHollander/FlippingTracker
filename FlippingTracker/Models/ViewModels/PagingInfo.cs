36e0d21c696c92cb6803ee3bab5bd10637e346f2
Jos Hollander <jos.hollander@ogd.nl>
2018-04-05 10:40:15 +0200

    Add PagingInfo viewmodel

--- FlippingTracker/Models/ViewModels/PagingInfo.cs
+++ FlippingTracker/Models/ViewModels/PagingInfo.cs
@@ -10,6 +10,6 @@ namespace FlippingTracker.Models.ViewModels
         public int TotalItems { get; set; }
         public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
-
+        public int TotalPages => (int) Math.Ceiling((decimal) TotalItems / ItemsPerPage);
     }
 }
